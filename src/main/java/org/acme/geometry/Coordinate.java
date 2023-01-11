228de3684463bbca9db9ca6d1e0ec1b34a536ee6
fernando <fernando.rico.95@outlook.com>
2023-01-11 16:39:09 +0100

    first part

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,5 +1,25 @@
 package org.acme.geometry;
 
 public class Coordinate {
+private final double x;
+private final double y;
+
+public Coordinate() {
+	this.x = 0.0;
+	this.y = 0.0;
+};
+
+public Coordinate(double x, double y) {
+	this.x = x;
+	this.y = y;
+}
+
+double getX() {
+	return this.x;
+	
+}
+double getY( ) {
+	return this.y;
+}
 
 }
